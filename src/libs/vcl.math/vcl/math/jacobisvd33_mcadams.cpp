b2844a270ba12807c784600356d69dbe5081e069
Basil Fierz <basil.fierz@outlook.com>
2015-02-19 23:13:21 +0100

    Adding the McAdams SVD code

---
+++ src/libs/vcl.math/vcl/math/jacobisvd33_mcadams.cpp
@@ -0,0 +1,90 @@
+/*
+ * This file is part of the Visual Computing Library (VCL) release under the
+ * MIT license.
+ *
+ * Copyright (c) 2015 Basil Fierz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include <vcl/math/jacobisvd33_mcadams.h>
+
+// VCL
+#include <vcl/core/contract.h>
+
+// McAdams SVD library
+#define USE_SCALAR_IMPLEMENTATION
+
+// #define USE_ACCURATE_RSQRT_IN_JACOBI_CONJUGATION
+// #define PERFORM_STRICT_QUATERNION_RENORMALIZATION
+// #define PRINT_DEBUGGING_OUTPUT
+
+#define COMPUTE_V_AS_MATRIX
+// #define COMPUTE_V_AS_QUATERNION
+#define COMPUTE_U_AS_MATRIX
+// #define COMPUTE_U_AS_QUATERNION
+
+#include <vcl/math/mcadams/Singular_Value_Decomposition_Preamble.hpp>
+
+namespace Vcl { namespace Mathematics
+{
+	int McAdamsJacobiSVD(Eigen::Matrix<float, 3, 3>& A, Eigen::Matrix<float, 3, 3>& U, Eigen::Matrix<float, 3, 3>& V)
+	{
+		using ::sqrt;
+
+#include <vcl/math/mcadams/Singular_Value_Decomposition_Kernel_Declarations.hpp>
+
+		ENABLE_SCALAR_IMPLEMENTATION(Sa11.f = A(0, 0);)
+		ENABLE_SCALAR_IMPLEMENTATION(Sa21.f = A(1, 0);)
+		ENABLE_SCALAR_IMPLEMENTATION(Sa31.f = A(2, 0);)
+		ENABLE_SCALAR_IMPLEMENTATION(Sa12.f = A(0, 1);)
+		ENABLE_SCALAR_IMPLEMENTATION(Sa22.f = A(1, 1);)
+		ENABLE_SCALAR_IMPLEMENTATION(Sa32.f = A(2, 1);)
+		ENABLE_SCALAR_IMPLEMENTATION(Sa13.f = A(0, 2);)
+		ENABLE_SCALAR_IMPLEMENTATION(Sa23.f = A(1, 2);)
+		ENABLE_SCALAR_IMPLEMENTATION(Sa33.f = A(2, 2);)
+
+#include <vcl/math/mcadams/Singular_Value_Decomposition_Main_Kernel_Body.hpp>
+
+		ENABLE_SCALAR_IMPLEMENTATION(U(0, 0) = Su11.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(U(1, 0) = Su21.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(U(2, 0) = Su31.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(U(0, 1) = Su12.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(U(1, 1) = Su22.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(U(2, 1) = Su32.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(U(0, 2) = Su13.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(U(1, 2) = Su23.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(U(2, 2) = Su33.f;)
+
+		ENABLE_SCALAR_IMPLEMENTATION(V(0, 0) = Sv11.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(V(1, 0) = Sv21.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(V(2, 0) = Sv31.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(V(0, 1) = Sv12.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(V(1, 1) = Sv22.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(V(2, 1) = Sv32.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(V(0, 2) = Sv13.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(V(1, 2) = Sv23.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(V(2, 2) = Sv33.f;)
+
+		ENABLE_SCALAR_IMPLEMENTATION(A(0, 0) = Sa11.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(A(1, 1) = Sa22.f;)
+		ENABLE_SCALAR_IMPLEMENTATION(A(2, 2) = Sa33.f;)
+
+		return 15;
+	}
+}}
---
+++ src/libs/vcl.math/vcl/math/jacobisvd33_mcadams_sse.cpp
@@ -0,0 +1,90 @@
+/*
+ * This file is part of the Visual Computing Library (VCL) release under the
+ * MIT license.
+ *
+ * Copyright (c) 2015 Basil Fierz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include <vcl/math/jacobisvd33_mcadams.h>
+
+// VCL
+#include <vcl/core/contract.h>
+
+// McAdams SVD library
+#define USE_SSE_IMPLEMENTATION
+
+// #define USE_ACCURATE_RSQRT_IN_JACOBI_CONJUGATION
+// #define PERFORM_STRICT_QUATERNION_RENORMALIZATION
+// #define PRINT_DEBUGGING_OUTPUT
+
+#define COMPUTE_V_AS_MATRIX
+// #define COMPUTE_V_AS_QUATERNION
+#define COMPUTE_U_AS_MATRIX
+// #define COMPUTE_U_AS_QUATERNION
+
+#include <vcl/math/mcadams/Singular_Value_Decomposition_Preamble.hpp>
+
+namespace Vcl { namespace Mathematics
+{
+	int McAdamsJacobiSVD(Eigen::Matrix<float4, 3, 3>& A, Eigen::Matrix<float4, 3, 3>& U, Eigen::Matrix<float4, 3, 3>& V)
+	{
+		using ::sqrt;
+
+#include <vcl/math/mcadams/Singular_Value_Decomposition_Kernel_Declarations.hpp>
+
+		ENABLE_SSE_IMPLEMENTATION(Va11 = _mm_loadu_ps((float*) &A(0, 0));)
+		ENABLE_SSE_IMPLEMENTATION(Va21 = _mm_loadu_ps((float*) &A(1, 0));)
+		ENABLE_SSE_IMPLEMENTATION(Va31 = _mm_loadu_ps((float*) &A(2, 0));)
+		ENABLE_SSE_IMPLEMENTATION(Va12 = _mm_loadu_ps((float*) &A(0, 1));)
+		ENABLE_SSE_IMPLEMENTATION(Va22 = _mm_loadu_ps((float*) &A(1, 1));)
+		ENABLE_SSE_IMPLEMENTATION(Va32 = _mm_loadu_ps((float*) &A(2, 1));)
+		ENABLE_SSE_IMPLEMENTATION(Va13 = _mm_loadu_ps((float*) &A(0, 2));)
+		ENABLE_SSE_IMPLEMENTATION(Va23 = _mm_loadu_ps((float*) &A(1, 2));)
+		ENABLE_SSE_IMPLEMENTATION(Va33 = _mm_loadu_ps((float*) &A(2, 2));)
+
+#include <vcl/math/mcadams/Singular_Value_Decomposition_Main_Kernel_Body.hpp>
+
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &U(0, 0), Vu11);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &U(1, 0), Vu21);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &U(2, 0), Vu31);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &U(0, 1), Vu12);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &U(1, 1), Vu22);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &U(2, 1), Vu32);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &U(0, 2), Vu13);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &U(1, 2), Vu23);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &U(2, 2), Vu33);)
+
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &V(0, 0), Vv11);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &V(1, 0), Vv21);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &V(2, 0), Vv31);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &V(0, 1), Vv12);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &V(1, 1), Vv22);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &V(2, 1), Vv32);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &V(0, 2), Vv13);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &V(1, 2), Vv23);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &V(2, 2), Vv33);)
+
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &A(0, 0), Va11);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &A(1, 1), Va22);)
+		ENABLE_SSE_IMPLEMENTATION(_mm_storeu_ps((float*) &A(2, 2), Va33);)
+
+		return 15;
+	}
+}}
