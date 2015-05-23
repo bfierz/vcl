ae43dee720f833f71ceddbb5360dfda76c1e2bf8
Basil Fierz <bfierz@users.noreply.github.com>
2015-05-23 18:13:36 +0200

    Adds a new unique pointer type with client pointers tracking lifetime of the owner

---
+++ src/libs/vcl.core/vcl/core/memory/smart_ptr.h
@@ -0,0 +1,166 @@
+/* 
+ * This file is part of the Visual Computing Library (VCL) release under the
+ * MIT license.
+ *
+ * Copyright (c) 2014-2015 Basil Fierz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#pragma once
+
+// VCL configuration
+#include <vcl/config/global.h>
+
+// C++ standard library
+#include <atomic>
+#include <cstdint>
+#include <memory>
+
+// VCL
+#include <vcl/core/contract.h>
+
+namespace Vcl { namespace Core
+{
+	class ref_cnt
+	{
+	public:
+		ref_cnt()
+		{
+			_references = false;
+		}
+
+		void setValid()
+		{
+			_references = true;
+		}
+		void setInvalid()
+		{
+			_references = false;
+		}
+
+		bool valid() const
+		{
+			return _references;
+		}
+
+	private:
+		std::atomic_bool _references;
+	};
+
+	template<typename T>
+	class owner_ptr
+	{
+		template<typename T> friend class ref_ptr;
+	public:
+		owner_ptr() = default;
+		owner_ptr(T* ptr)
+		: _ptr(ptr)
+		{
+			_cnt = std::make_shared<ref_cnt>();
+			_cnt->setValid();
+		}
+		owner_ptr(const owner_ptr&) = delete;
+		owner_ptr(owner_ptr&& rhs)
+		{
+			std::swap(_ptr, rhs._ptr);
+			std::swap(_cnt, rhs._cnt);
+		}
+
+		~owner_ptr()
+		{
+			_cnt->setInvalid();
+			_ptr = nullptr;
+		}
+
+		void reset(T* ptr = nullptr)
+		{
+			_cnt = std::make_shared<ref_cnt>();
+			_cnt->setValid();
+			_ptr = ptr;
+		}
+
+		operator bool() const
+		{
+			return _ptr;
+		}
+
+		T* operator->() const
+		{
+			return _ptr;
+		}
+
+		T* get() const { return _ptr; }
+
+	public: // Access
+		long use_count() const
+		{
+			return _cnt.use_count() - 1;
+		}
+
+	private:
+		T* _ptr{ nullptr };
+		std::shared_ptr<ref_cnt> _cnt;
+	};
+
+	template<typename T>
+	class ref_ptr
+	{
+	public:
+		ref_ptr() = default;
+
+		ref_ptr(const owner_ptr<T>& ptr)
+		: _ptr(ptr.get())
+		, _cnt(ptr._cnt)
+		{
+		}
+
+		~ref_ptr()
+		{
+			_ptr = nullptr;
+		}
+
+		void reset()
+		{
+			_cnt.reset();
+			_ptr = nullptr;
+		}
+
+		void reset(const owner_ptr<T>& ptr)
+		{
+			_cnt = ptr._cnt;
+			_ptr = ptr.get();
+		}
+
+		operator bool() const
+		{
+			return _ptr && _cnt;
+		}
+
+		T* operator->() const
+		{
+			return _ptr;
+		}
+
+		T* get() const { return _ptr; }
+
+	private:
+		T* _ptr{ nullptr };
+		std::shared_ptr<ref_cnt> _cnt;
+	};
+}}
---
+++ src/tests/vcl.core/smart_ptr.cpp
@@ -0,0 +1,70 @@
+/*
+ * This file is part of the Visual Computing Library (VCL) release under the
+ * MIT license.
+ *
+ * Copyright (c) 2015 Basil Fierz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// VCL configuration
+#include <vcl/config/global.h>
+
+// Include the relevant parts from the library
+#include <vcl/core/memory/smart_ptr.h>
+
+// C++ standard library
+
+// Google test
+#include <gtest/gtest.h>
+
+TEST(OwnerPtrTest, Simple)
+{
+	using namespace Vcl::Core;
+
+	int i = 5;
+
+	// Owner and ref in same scope
+	{
+		owner_ptr<int> owner{ &i };
+		ref_ptr<int> ref{ owner };
+
+		EXPECT_EQ(owner.get(), ref.get());
+		EXPECT_TRUE(ref);
+		EXPECT_EQ(1, owner.use_count());
+
+		ref_ptr<int> ref2{ owner };
+		EXPECT_EQ(2, owner.use_count());
+	}
+
+	// Owner and ref in different scopes
+	{
+		ref_ptr<int> ref;
+		{
+			owner_ptr<int> owner{ &i };
+			ref.reset(owner);
+
+			EXPECT_EQ(owner.get(), ref.get());
+			EXPECT_TRUE(ref);
+			EXPECT_EQ(1, owner.use_count());
+		}
+		
+		EXPECT_TRUE(ref);
+	}
+}
