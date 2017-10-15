2f4c33d5d8ac33289950c829a273cd28350b3892
Basil Fierz <basil.fierz@hotmail.com>
2017-10-15 21:38:21 +0200

    Initial OBB support

---
+++ src/libs/vcl.geometry/vcl/geometry/primitives/obb.h
@@ -0,0 +1,128 @@
+/*
+ * This file is part of the Visual Computing Library (VCL) release under the
+ * MIT license.
+ *
+ * Copyright (c) 2017 Basil Fierz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#pragma once
+
+// VCL configuration
+#include <vcl/config/global.h>
+#include <vcl/config/eigen.h>
+
+// Eigen
+#include <Eigen/Dense>
+
+// GSL
+#include <gsl/gsl>
+
+namespace Vcl { namespace Geometry
+{
+	//! Fitting of bounding box based on PhD thesis of Stefan Gottschalk
+	//! http://gamma.cs.unc.edu/users/gottschalk/main.pdf
+	//! Summarized by James Gregson
+	//! http://jamesgregson.blogspot.ch/2011/03/latex-test.html
+	template<typename Scalar, int Dim>
+	class OrientedBox
+	{
+	public: // Type aliases
+		using real_t = Scalar;
+		using vector_t = Eigen::Matrix<Scalar, Dim, 1>;
+		using matrix_t = Eigen::Matrix<Scalar, Dim, Dim>;
+
+	public:
+		OrientedBox(gsl::span<const vector_t> points)
+		{
+			constructFromPoints(points);
+		}
+
+		const vector_t& center() const { return _center; }
+
+	private: // Construction
+		void constructFromPoints(gsl::span<const vector_t> points)
+		{
+			// Find the mean point
+			vector_t mu = vector_t::Zero();
+			const real_t size = static_cast<real_t>(points.size());
+			const real_t inv_size = 1 / size;
+			for (const auto& p : points)
+			{
+				mu += p * inv_size;
+			}
+
+			// Build the co-variance matrix
+			real_t cxx = -size * mu.x()*mu.x();
+			real_t cxy = -size * mu.x()*mu.y();
+			real_t cxz = -size * mu.x()*mu.z();
+			real_t cyy = -size * mu.y()*mu.y();
+			real_t cyz = -size * mu.y()*mu.z();
+			real_t czz = -size * mu.z()*mu.z();
+			for (const auto& p : points)
+			{
+				cxx += p.x()*p.x();
+				cxy += p.x()*p.y();
+				cxz += p.x()*p.z();
+				cyy += p.y()*p.y();
+				cyz += p.y()*p.z();
+				czz += p.z()*p.z();
+			}
+
+			matrix_t C = matrix_t::Zero();
+			C(0, 0) = cxx; C(0, 1) = cxy; C(0, 2) = cxz;
+			C(1, 0) = cxy; C(1, 1) = cyy; C(1, 2) = cyz;
+			C(2, 0) = cxz; C(2, 1) = cyz; C(2, 2) = czz;
+
+			evaluateCovariance(points, C);
+		}
+
+		void evaluateCovariance(gsl::span<const vector_t> points, const matrix_t& C)
+		{
+			Eigen::SelfAdjointEigenSolver<matrix_t> solver;
+			solver.compute(C, Eigen::ComputeEigenvectors);
+
+			_orientation = solver.eigenvectors();
+			_orientation.col(0).normalize();
+			_orientation.col(1).normalize();
+			_orientation.col(2).normalize();
+
+			// Transform the points the new basis defined by 'rot'
+			// and find the minimum and maximum
+			Eigen::AlignedBox<Scalar, Dim> box;
+			for (const auto& p : points)
+			{
+				box.extend(_orientation.transpose() * p);
+			}
+
+			_center = box.center();
+			_extents = box.sizes() / 2;
+		}
+
+	private: // Data
+		//! Center of the oriented box
+		vector_t _center;
+
+		//! Extent of the oriented box along the defined axes
+		vector_t _extents;
+
+		//! Orientation of the box, each column defines one axis
+		matrix_t _orientation;
+	};
+}}
---
+++ src/tests/vcl.geometry/obb.cpp
@@ -0,0 +1,72 @@
+/*
+ * This file is part of the Visual Computing Library (VCL) release under the
+ * MIT license.
+ *
+ * Copyright (c) 2017 Basil Fierz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// VCL configuration
+#include <vcl/config/global.h>
+#include <vcl/config/eigen.h>
+
+// C++ Standard Library
+
+// Include the relevant parts from the library
+#include <vcl/geometry/primitives/obb.h>
+
+// Google test
+#include <gtest/gtest.h>
+
+// Local
+#include "liver_766.h"
+
+TEST(OrientedBoxTest, SimpleConstruction)
+{
+	using namespace Vcl::Geometry;
+	
+	// Basic box
+	Eigen::Vector3f min_pos = Eigen::Vector3f::Constant(-1);
+	Eigen::Vector3f max_pos = Eigen::Vector3f::Constant( 1);
+	Eigen::AlignedBox3f aligned_box{ min_pos, max_pos };
+	
+	// Define random rotation
+	Eigen::AngleAxisf rot{ 0.785f, Eigen::Vector3f::Random().normalized() };
+
+	size_t c = 0;
+	std::vector<Eigen::Vector3f> points;
+	std::generate_n(std::back_inserter(points), 8, [&aligned_box, &rot, &c]()
+	{
+		return rot * aligned_box.corner(static_cast<Eigen::AlignedBox3f::CornerType>(c++));
+	});
+	
+	OrientedBox<float, 3> obb{ points };
+
+	EXPECT_FLOAT_EQ(obb.center().x(), 0);
+	EXPECT_FLOAT_EQ(obb.center().y(), 0);
+	EXPECT_FLOAT_EQ(obb.center().z(), 0);
+}
+
+TEST(OrientedBoxTest, ConstructionFromMesh)
+{
+	using namespace Vcl::Geometry;
+
+	OrientedBox<float, 3> obb{ { reinterpret_cast<const Eigen::Vector3f*>(liver_766_points), num_liver_766_points / 3 } };
+}
