4df2383d9feb60c6049f6b2313302d5e160ddbfb
Basil Fierz <basil.fierz@hotmail.com>
2018-08-08 20:26:21 +0200

    Configure core guidelines checker

--- src/cmake/VCLConfig.cmake
+++ src/cmake/VCLConfig.cmake
@@ -126,8 +126,9 @@ function(vcl_configure tgt)
 		# * Enable all warnings
 		# * Exceptions
 		# * RTTI
+		# * Don't be permissive
 		target_compile_options(${tgt} PUBLIC "/EHsc" "/GR")
-		target_compile_options(${tgt} PRIVATE "/W4")
+		target_compile_options(${tgt} PRIVATE "/W4" "/permissive-")
 	
 		# Make AVX available
 		if(VCL_VECTORIZE_AVX2)
@@ -171,12 +172,62 @@ function(vcl_configure tgt)
 	endif(VCL_COMPILER_GNU OR VCL_COMPILER_CLANG OR VCL_COMPILER_ICC)
 endfunction()
 
+# Travers all targets
+function(get_include_paths OUTPUT_LIST TARGET)
+    list(APPEND VISITED_TARGETS "${TARGET}")
+
+	# Determine target type. IMPORTED and INTERFACE_LIBRARY only support
+	# a restricted interface.
+    get_target_property(IMPORTED "${TARGET}" IMPORTED)
+    get_target_property(TYPE "${TARGET}" TYPE)
+
+	# Query the possible links for the recursive search
+    if (IMPORTED OR TYPE STREQUAL "INTERFACE_LIBRARY")
+        get_target_property(TARGETS "${TARGET}" INTERFACE_LINK_LIBRARIES)
+    else()
+        get_target_property(TARGETS "${TARGET}" LINK_LIBRARIES)
+    endif()
+
+    set(INCLUDE_PATHS "")
+    foreach(TGT ${TARGETS})
+        if (TARGET ${TGT})
+            list(FIND VISITED_TARGETS ${TGT} VISITED)
+            if (${VISITED} EQUAL -1)
+				get_target_property(TGT_INC_PATHS ${TGT} INTERFACE_INCLUDE_DIRECTORIES)
+				foreach(PATH ${TGT_INC_PATHS})
+					string(FIND ${PATH} ${PROJECT_SOURCE_DIR} FOUND)
+					if (EXISTS ${PATH} AND ${FOUND} EQUAL -1)
+						list(APPEND INCLUDE_PATHS ${PATH} ${DEPENDENCIES_INC_PATHS})
+					endif()
+				endforeach()
+                get_include_paths(DEPENDENCIES_INC_PATHS ${TGT})
+            endif()
+        endif()
+    endforeach()
+    set(VISITED_TARGETS ${VISITED_TARGETS} PARENT_SCOPE)
+	list(REMOVE_DUPLICATES INCLUDE_PATHS)
+    set(${OUTPUT_LIST} ${INCLUDE_PATHS} PARENT_SCOPE)
+endfunction()
+
 # Function enabling the Core guideline checker from Visual Studio
 function(enable_vs_guideline_checker target)
+	get_include_paths(target_include_paths ${target})
+
+	if (MSVC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 19.13)
+		# Exclude external headers from static code analysis. According to a remark in
+		# https://blogs.msdn.microsoft.com/vcblog/2017/12/13/broken-warnings-theory/
+		# only the environment variabl 'CAExcludePath' seems to work.
+		target_compile_options(${target} PRIVATE "/experimental:external" "/external:env:CAExcludePath")
+	endif()
+
 	set_target_properties(${target} PROPERTIES
 		VS_GLOBAL_EnableCppCoreCheck true
 		VS_GLOBAL_CodeAnalysisRuleSet CppCoreCheckRules.ruleset
+		VS_GLOBAL_CAExcludePath "${target_include_paths}"
 		VS_GLOBAL_RunCodeAnalysis true)
+		
+	# Pass the information about the core guidelines checker to the target
+	target_compile_definitions(${target} PRIVATE VCL_CHECK_CORE_GUIDELINES)
 endfunction()
 
 # Checks if a target with a given names exists
