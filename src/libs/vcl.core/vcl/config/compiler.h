9671d5f4b40d4f18c5ad8f988e4fdecdc1c98d3d
Rafael Stahl <dummdoof-doof@web.de>
2025-02-25 09:12:07 +0000

    fix for android toolchain

--- src/libs/vcl.core/vcl/config/compiler.h
+++ src/libs/vcl.core/vcl/config/compiler.h
@@ -24,6 +24,9 @@
  */
 #pragma once
 
+// For checking __CLANG_MAX_ALIGN_T_DEFINED
+#include <cstddef>
+
 #include <vcl/config/config.h>
 
 // Check library configuration
@@ -201,7 +204,7 @@
 // Add missing definition for max_align_t for compatibility with older clang version (3.4, 3.5)
 #	if defined(VCL_COMPILER_CLANG) && !defined(_MSC_VER) && !defined(__APPLE_CC__)
 #		if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L) || __cplusplus >= 201103L
-#			if !defined(__CLANG_MAX_ALIGN_T_DEFINED) && !defined(_GCC_MAX_ALIGN_T) && !defined(__DEFINED_max_align_t) && !defined(__EMSCRIPTEN__)
+#			if !defined(__CLANG_MAX_ALIGN_T_DEFINED) && !defined(_GCC_MAX_ALIGN_T) && !defined(__DEFINED_max_align_t)
 typedef struct
 {
 	long long __clang_max_align_nonce1 __attribute__((__aligned__(__alignof__(long long))));
